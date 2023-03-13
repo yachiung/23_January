97462ee2cd61ab238d6eebcb0cc2f153bc27fe7f
yachiung <chiung@hyweb.com.tw>
2023-03-13 19:04:03 +0800

    tab glide 調整

---
+++ js/uieffect.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  var _window = $(window);
+
+
+  var _sidebarCtrl = $('.sidebarCtrl');
+
+  _sidebarCtrl.click(function(){
+    _this = $(this);
+    if (_this.hasClass('closeIt')) {
+      _this.removeClass('closeIt')
+    } else {
+      _this.addClass('closeIt');
+    }
+  })
+
+  var _searchCtrl = $('.searchCtrl');
+  var _siteSearch = $('.siteSearch');
+
+  _searchCtrl.click( function(){
+    _siteSearch.hasClass('show') ? _siteSearch.removeClass('show') :  _siteSearch.addClass('show');
+    // _searchCtrl.hasClass('closeIt')  ? _searchCtrl.removeClass('closeIt') : _searchCtrl.addClass('closeIt');
+
+  })
+
+})
